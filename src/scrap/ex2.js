81a5029191c6693f015413c92fb4c282a1b9da2c
frandeer <heyhey482@gmail.com>
2022-12-28 00:08:40 +0900

    식당 3건

--- src/scrap/ex2.js
+++ src/scrap/ex2.js
@@ -7,5 +7,13 @@ axios.get('https://today.jnu.ac.kr/Page/GetHtmlPage2.aspx').then((res) => {
   const $ = cheerio.load(htmlString);
   const h1 = $('.T_tableA').eq(1).find('tr').eq(1);
   console.log(h1.text());
+
+
+  const h2 = $('.T_tableA').eq(2).find('tr').eq(1);
+  console.log(h2.text());
   // console.log(htmlString);
+
+  const h3 = $('.T_tableA').eq(4).find('tr').eq(1);
+  console.log(h3.text());
 });
+
