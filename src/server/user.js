9afcfce4550c26c92bd7372e5937dfec326504cd
hogle <ho@wedb.co.kr>
2022-12-28 17:56:28 +0900

    user-profile

--- src/server/user.js
+++ src/server/user.js
@@ -1,24 +1,51 @@
 const express = require('express');
-const userRouter = express.Router()
+const router = express.Router()
 
-const users = [
-  { id: 1, name: 'John' },
-  { id: 2, name: 'Jane' },
-]
+const multer = require('multer');
 
-userRouter.get('/', (req, res, next) => {
+const upload = multer({ dest: 'uploads/' })
+
+const users = {
+  1: { 
+    id: 1, 
+    name: 'John',
+    avatar: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50',
+  },
+  2: { 
+      id: 2, 
+      name: 'Jane',
+      avatar: undefined,
+  },
+}
+
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hey', message: 'Hello there!', users })
 });
 
-userRouter.get('/profile', (req, res, next) => {
-  res.json({
-    name: 'John',
-  })
+router.get('/:id/name', (req, res, next) => {
+  const { id } = req.params;
+  const user = users[id];
+  res.send(user);
 });
+
+router.get('/:id/profile', (req, res, next) => {
+ res.render('user-profile', { user: users[req.params.id] });
+});
+
+router.post('/:id/avatar', upload.single('avatar'), (req, res, next) => {
+  const { id } = req.params;
+  const { file } = req;
+  const user = users[id];
+  user.avatar = file.path;
+  res.redirect(`/user/${id}`);
+});
+
+
+
 
 // userRouter.get('/:id', (req, res, next) => {
 //   res.send(`user param: ${req.params.id}`);
 // });
 
 
-module.exports = userRouter;
+module.exports = router;
