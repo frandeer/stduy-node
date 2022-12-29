570693f214b35b8b725159c3aa6080bfe0246cbb
hogle <ho@wedb.co.kr>
2022-12-29 13:24:50 +0900

    path update

--- src/server/menu.js
+++ src/server/menu.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router()
 
 const fs = require('fs');
+const path = require('path');
 
 // menu.json 파일을 읽어서 menuList에 저장
 router.get('/', (req, res, next) => {
-  const menuList = JSON.parse(fs.readFileSync('src/data/menu.json'));
+  const menuList = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/menu.json'), 'utf-8'));
   res.render('menu', { menuList });
 });
 
