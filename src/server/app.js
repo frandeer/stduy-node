0853286b985dcb2fd09f77475ee9b7119b769b24
hogle <ho@wedb.co.kr>
2022-12-29 13:28:05 +0900

    path

--- src/server/app.js
+++ src/server/app.js
@@ -2,12 +2,13 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.use(express.json())
-app.use('/assets', express.static('src/public'))
-app.use('/uploads', express.static('uploads'))
+app.use('/assets', express.static(path.join(__dirname, '../public')))
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 // pug
-app.set('views', 'src/views');
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 const userRouter = require('./user');
