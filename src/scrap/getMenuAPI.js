84d2dd4d87e38545c62165fd28eb5495df868ebf
ho <ho@wedb.kr>
2022-12-30 17:55:12 +0900

    error json write

--- src/scrap/getMenuAPI.js
+++ src/scrap/getMenuAPI.js
@@ -7,6 +7,7 @@ const getImage = require('./naverAPI');
 const getMenu = require('./getMenu');
 
 const fs = require('fs');
+const path = require('path');
 
 function start() {
   getMenu().then((res) => {
@@ -38,9 +39,7 @@ function start() {
     })
 
     Promise.all(promises).then((data) => {
-      console.log('====================================');
-      console.log(data);
-      console.log('====================================');
+      // console.log(data);
 
       data.push({
         name: '내일의 한식',
@@ -57,7 +56,7 @@ function start() {
       })
 
 
-      fs.writeFileSync('src/data/menu.json', JSON.stringify(data));
+      fs.writeFileSync(path.join(__dirname, '../data/menu.json'), JSON.stringify(data));
     }).catch((err) => {
         console.log(err);
     })
