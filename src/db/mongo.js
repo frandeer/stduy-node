da25373bcb19f118a09abbb7e4c0f597fba2d373
ho <ho@wedb.co.kr>
2022-12-31 14:43:24 +0900

    update

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://frandeer:mxlRAREcxRZvYvRR@cluster0.qthnw4r.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.qthnw4r.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
