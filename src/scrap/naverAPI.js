62b1b1ed1c6db7062d4a1c26f5ac0255f114c5ea
ho <ho@wedb.kr>
2023-01-04 10:56:42 +0900

    naver query error

--- src/scrap/naverAPI.js
+++ src/scrap/naverAPI.js
@@ -14,12 +14,31 @@ const naverAPI = axios.create({
 
 function getImage(data, duration) {
   return new Promise((resolve, reject) => {
+
+    if(!data.name) {
+      new Error("Not Query.")
+      reject('error')
+    }
+
     setTimeout(() => {
       naverAPI.get(`/v1/search/image?display=1&query=${data.name}`).then((res) => {
+
+        let thumbnail = ''
+        let link = ''
+
+        if(res.data.items[0] !== undefined) {
+          if(typeof res.data.items[0].thumbnail !== undefined) {
+            thumbnail = res.data.items[0].thumbnail
+          }
+          if(typeof res.data.items[0].link !== undefined) {
+            link = res.data.items[0].link
+          }
+        }
+
         resolve({
           name: data.name,
-          image: res.data.items[0].thumbnail,
-          image2: res.data.items[0].link,
+          image: thumbnail,
+          image2: link,
           type: data.type,
           date: data.date,
         })
