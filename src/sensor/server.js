ee826a0950845396ed0e56ba076fa99e32df184d
frandeer <heyhey482@gmail.com>
2023-01-28 14:15:34 +0900

    sensor server

--- src/sensor/server.js
+++ src/sensor/server.js
@@ -1,5 +1,20 @@
 const constants = require('./constants');
 const express = require('express');
 const app = express();
-
+const path = require('path');
+const cors = require('cors');
+const http = require('http').Server(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+  }
+});
 const log = console.log
+
+const mongo = require('./mongo');
+let userList = [], idx = 0;
+
+
+
+
+
